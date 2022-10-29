a1c6c062bf7e2db2a08355a341d8fff1261446f7
EC2 Default User <ec2-user@ip-172-31-8-66.ap-northeast-1.compute.internal>
2022-10-29 08:12:54 +0000

    Reform js_code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //入力データを保管するために、変数resultを設定。初期値をnullとする。
 let result = "";
-//＝で計算したかを判別するために設定。数字キー押下時の分岐条件として用いる。初期値をflaseとする。
+//直前の入力が＝であるかを判別するために設定。数字キー押下時の分岐条件として用いる。初期値をflaseとする。
 let is_calc = false;
 //DOMのレンダリング完了後、変数result、にid="result"を含むドキュメント要素（以後頻繁に使う）を代入する。
 //以後は後者と同意味の前者を使用し、コードを見やすくしている。
@@ -16,6 +16,8 @@ function c_click() {
 
 //数字キー押下時の処理。引数valにはhtmlで設定した値が代入される（今回は this.innerHTML　のみ）。
 function num_click(val) {
+  
+  //直前の入力が=であるとき、前データを消去し、"0"を新たなデータとする。
   if(is_calc) result.value = "0"
   is_calc = false;
   
@@ -24,7 +26,7 @@ function num_click(val) {
     result.value = result.value;
   }
   //"."はデータ中に１つのみとする
-  else if(result.value.toString().includes(".") && val == "."){//toStringが不要な気がするのであとで消去して問題ないか調べてみる
+  else if(result.value.includes(".") && val == "."){
     result.value = result.value;
   }
   //前データが"0" かつ直後の入力値が"." の時のみ 前データ("0")を保管。それに入力値（"."）を加えたものを新たなデータとする
@@ -39,19 +41,17 @@ function num_click(val) {
   else {
     result.value = result.value + val;
   }
-  
-    
 }
 
 //データの末尾が演算子か判別するために設定。演算子キー、＝キー押下の処理の時に用いる。
 function is_ope_last() {
-  return ["+", "-", "×", "÷"].includes(result.value.toString().slice(-1)); ///toStringが不要な気がするのであとで消去して問題ないか調べて
+  return ["+", "-", "×", "÷"].includes(result.value.slice(-1));
 }
 
 
 //演算子キー押下時の処理
 function ope_click(val) {
-  if(is_calc) is_calc = false; //if文つける必要ないと思う。is_calc = falseだけでいけると思うから後で修正
+  is_calc = false;
   
   //データに演算子が連続することを防ぐ
   if(is_ope_last()){
@@ -60,7 +60,6 @@ function ope_click(val) {
   else {
     result.value = result.value + val;
   }
-  
 }
 
 //=キー押下時の時の処理
@@ -76,6 +75,5 @@ function equal_click() {
     result.value = temp;
     is_calc = true;
   }
-  
 }
 
